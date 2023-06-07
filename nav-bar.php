b7c1a97b2493b945666d96f5e5bd5a4130a46e1b
Knowen Obiso <knowenemmanuel@gmail.com>
2023-06-07 09:50:36 +0300

    first-commit

--- nav-bar.php
+++ nav-bar.php
@@ -1,14 +1,14 @@
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <div class="container">
-            <a class="navbar-brand" href="/">FitKonnect</a>
+            <a class="navbar-brand" href="http://localhost/fitkonect/">FitKonnect</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item active">
-                        <a class="nav-link" href="/">Home</a>
+                        <a class="nav-link" href="http://localhost/fitkonect/">Home</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Book a Session</a>
@@ -22,7 +22,7 @@
                 </ul>
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Login / Signup</a>
+                        <a class="nav-link" href="http://localhost/fitkonect/client/login.php">Login / Signup</a>
                     </li>
                 </ul>
             </div>
