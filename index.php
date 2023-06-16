0e36bf9f835139b813819389709427c4a1c9edf2
Knowen Obiso <knowenemmanuel@gmail.com>
2023-06-16 23:51:40 +0300

    uncommented code for the footer

--- index.php
+++ index.php
@@ -17,7 +17,8 @@ include 'carousels.php';
     <!-- Sliding Carousels -->
    
      
-    <?php //include 'footer.php'; ?>
+    <!-- footer -->
+    <?php include 'footer.php'; ?>
     <!-- jQuery and Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
